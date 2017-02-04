d31f732356beb8cfa69a7617b62fe38aea6ccd41
Lauriane <lauriane.b.michaud@gmail.com>
2017-02-04 14:36:39 -0500

    Initialisation api (frontend), template : http://startbootstrap.com/template-overviews/sb-admin-2/

---
+++ api/app/scripts/controllers_v2/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name apiApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the apiApp
+ */
+angular.module('apiApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ api/app/scripts/controllers_v2/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name apiApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the apiApp
+ */
+angular.module('apiApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
